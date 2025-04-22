f8241bdca06a07e4df0b37c4ac2325b52bac741e
5xJIN <pinion0801@gmail.com>
2025-04-23 01:33:01 +0900

    :sparkles: Add Storybook story for Typography component

---
+++ src/stories/Typography/Label.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+
+import { defaultTheme } from '@/theme';
+import { Typography } from '@/Typography';
+
+const meta: Meta<typeof Typography.Label> = {
+  title: 'Typography/Label',
+  component: Typography.Label,
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={defaultTheme}>
+        <Story />
+      </ThemeProvider>
+    )
+  ],
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['xsm', 'sm', 'md', 'lg']
+    },
+    color: {
+      control: 'color'
+    },
+    fontWeight: {
+      control: 'number'
+    }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Typography.Label>;
+
+export const Label: Story = {
+  args: {
+    size: 'md',
+    fontWeight: 400,
+    color: 'rgba(0, 0, 0, 0.85)',
+    children: 'Display Bold'
+  }
+};
