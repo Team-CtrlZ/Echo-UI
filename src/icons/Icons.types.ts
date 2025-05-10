79cafd67813d26416bf8663cd5effe2f786ae247
Jin <pinion0801@gmail.com>
2025-05-10 23:10:48 +0900

    Release/0.2.0 (#8)
    * :bug: Fix build error
    
    * :wrench: Update exports field in package.json
    
    * 0.2.0
    
    * :wrench: Add branch condition to PR GitHub Action

--- src/icons/Icons.types.ts
+++ src/icons/Icons.types.ts
@@ -1,5 +1,39 @@
+import { Alerts } from '@/icons/categories/Alerts';
+import { Arrow } from '@/icons/categories/Arrow';
+import { Communication } from '@/icons/categories/Communication';
+import { Editor } from '@/icons/categories/Editor';
+import { Files } from '@/icons/categories/Files';
+import { Finance } from '@/icons/categories/Finance';
+import { General } from '@/icons/categories/General';
+import { Layout } from '@/icons/categories/Layout';
+import { Maps } from '@/icons/categories/Maps';
+import { Media } from '@/icons/categories/Media';
+import { Medical } from '@/icons/categories/Medical';
+import { Security } from '@/icons/categories/Security';
+import { Shapes } from '@/icons/categories/Shapes';
+import { Time } from '@/icons/categories/Time';
+import { Users } from '@/icons/categories/Users';
+
 export interface Icon {
   width: number;
   height: number;
   color: string;
+}
+
+export interface IconType {
+  Alerts: typeof Alerts;
+  Arrow: typeof Arrow;
+  Communication: typeof Communication;
+  Editor: typeof Editor;
+  Files: typeof Files;
+  Finance: typeof Finance;
+  General: typeof General;
+  Layout: typeof Layout;
+  Maps: typeof Maps;
+  Media: typeof Media;
+  Medical: typeof Medical;
+  Security: typeof Security;
+  Shapes: typeof Shapes;
+  Time: typeof Time;
+  Users: typeof Users;
 }
